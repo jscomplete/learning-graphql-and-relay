73745b9a163ddca1beb7e3a8317136c14d3bf8c6
Samer Buna <samer.buna@gmail.com>
2016-08-11 15:18:05 -0700

    Chapter 1: Rolling the dice

--- schema/main.js
+++ schema/main.js
@@ -1,8 +1,12 @@
 const {
   GraphQLSchema,
   GraphQLObjectType,
-  GraphQLString
+  GraphQLString,
+  GraphQLInt,
+  GraphQLList
 } = require('graphql');
+
+const roll = () => Math.floor(6 * Math.random()) + 1;
 
 const queryType = new GraphQLObjectType({
   name: 'RootQuery',
@@ -10,6 +14,10 @@ const queryType = new GraphQLObjectType({
     hello: {
       type: GraphQLString,
       resolve: () => 'world'
+    },
+    diceRoll: {
+      type: new GraphQLList(GraphQLInt),
+      resolve: () => [roll(), roll()]
     }
   }
 });
