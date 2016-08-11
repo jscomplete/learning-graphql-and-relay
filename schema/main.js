af4163dafd902676fc8728bf9f9f776bbd93af5c
Samer Buna <samer.buna@gmail.com>
2016-08-11 16:45:43 -0700

    Chapter 4: The quotes library

--- schema/main.js
+++ schema/main.js
@@ -8,14 +8,26 @@ const {
   GraphQLEnumType
 } = require('graphql');
 
+const QuoteType = new GraphQLObjectType({
+  name: 'Quote',
+  fields: {
+    id: {
+      type: GraphQLString,
+      resolve: obj => obj._id
+    },
+    text: { type: GraphQLString },
+    author: { type: GraphQLString }
+  }
+});
+
 const queryType = new GraphQLObjectType({
   name: 'RootQuery',
   fields: {
-    usersCount: {
-      description: 'Total number of users in the database',
-      type: GraphQLInt,
+    allQuotes: {
+      type: new GraphQLList(QuoteType),
+      description: 'A list of the quotes in the database',
       resolve: (_, args, { db }) =>
-        db.collection('users').count()
+        db.collection('quotes').find().toArray()
     }
   }
 });
