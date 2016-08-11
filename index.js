5343d3d130b54891cbd655a63f591603ee0ad8d4
Samer Buna <samer.buna@gmail.com>
2016-08-11 15:44:09 -0700

    Chapter 1: Setting up MongoDB

--- index.js
+++ index.js
@@ -8,10 +8,19 @@ const rli = readline.createInterface({
   output: process.stdout
 });
 
-rli.question('Client Request: ', inputQuery => {
-  graphql(mySchema, inputQuery).then(result => {
-    console.log('Server Answer :', result.data);
-  });
+const { MongoClient } = require('mongodb');
+const assert = require('assert');
 
-  rli.close();
+const MONGO_URL = 'mongodb://localhost:27017/test';
+
+MongoClient.connect(MONGO_URL, (err, db) => {
+  assert.equal(null, err);
+  console.log('Connected to MongoDB server');
+
+  rli.question('Client Request: ', inputQuery => {
+    graphql(mySchema, inputQuery, {}, { db }).then(result => {
+      console.log('Server Answer :', result.data);
+      db.close(() => rli.close());
+    });
+  });
 });
--- schema/main.js
+++ schema/main.js
@@ -30,6 +30,11 @@ const queryType = new GraphQLObjectType({
         }
         return rolls;
       }
+    },
+    usersCount: {
+      type: GraphQLInt,
+      resolve: (_, args, { db }) =>
+        db.collection('users').count()
     }
   }
 });
