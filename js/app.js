a7b27304aefc522a111fcece6cf1d0e1e5014188
Samer Buna <samer.buna@gmail.com>
2016-08-11 20:58:49 -0700

    Chapter 6: Implementing the search feature in Relay

--- js/app.js
+++ js/app.js
@@ -1,26 +1,41 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import Relay from 'react-relay';
+import { debounce } from 'lodash';
 
+import SearchForm from './search-form';
 import Quote from './quote';
 
 class QuotesLibrary extends React.Component {
+  constructor(props) {
+    super(props);
+    this.search = debounce(this.search.bind(this), 300);
+  }
+  search(searchTerm) {
+    this.props.relay.setVariables({searchTerm});
+  }
   render() {
     return (
-      <div className="quotes-list">
-        {this.props.library.quotesConnection.edges.map(edge =>
-          <Quote key={edge.node.id} quote={edge.node} />
-        )}
+      <div className="quotes-library">
+        <SearchForm searchAction={this.search} />
+        <div className="quotes-list">
+          {this.props.library.quotesConnection.edges.map(edge =>
+            <Quote key={edge.node.id} quote={edge.node} />
+          )}
+        </div>
       </div>
     )
   }
 }
 
 QuotesLibrary = Relay.createContainer(QuotesLibrary, {
+  initialVariables: {
+    searchTerm: ''
+  },
   fragments: {
     library: () => Relay.QL `
       fragment on QuotesLibrary {
-        quotesConnection(first: 100) {
+        quotesConnection(first: 100, searchTerm: $searchTerm) {
           edges {
             node {
               id
