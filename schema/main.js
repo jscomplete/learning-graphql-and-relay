5c6f64ca416a94ed7fef2d1f5e2b4abb64a39fbf
Samer Buna <samer.buna@gmail.com>
2016-08-11 20:55:22 -0700

    Chapter 6: Adding a search feature to the GraphQL API

--- schema/main.js
+++ schema/main.js
@@ -33,17 +33,26 @@ const { connectionType: QuotesConnectionType } =
     nodeType: QuoteType
   });
 
+let connectionArgsWithSearch = connectionArgs;
+connectionArgsWithSearch.searchTerm = { type: GraphQLString };
+
 const QuotesLibraryType = new GraphQLObjectType({
   name: 'QuotesLibrary',
   fields: {
     quotesConnection: {
       type: QuotesConnectionType,
       description: 'A list of the quotes in the database',
-      args: connectionArgs,
-      resolve: (_, args, { db }) => connectionFromPromisedArray(
-        db.collection('quotes').find().toArray(),
-        args
-      )
+      args: connectionArgsWithSearch,
+      resolve: (_, args, { db }) => {
+        let findParams = {};
+        if (args.searchTerm) {
+          findParams.text = new RegExp(args.searchTerm, 'i');
+        }
+        return connectionFromPromisedArray(
+          db.collection('quotes').find(findParams).toArray(),
+          args
+        );
+      }
     }
   }
 });
